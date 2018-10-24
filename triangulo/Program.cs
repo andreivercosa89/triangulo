1b27ee53fc3fd54f370aaf61d183d93a004de5b2
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-23 20:39:20 -0400

    triangulo

---
+++ triangulo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace triangulo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int ladoA;
+            int ladoB;
+            int ladoC;
+
+            string texto;
+
+            /*Console.WriteLine("Digite qualquer coisa...");
+            texto = "Foi digitado isso aqui: " + Console.ReadLine();
+            Console.WriteLine(texto);*/
+
+            Console.WriteLine("Digite um valor para o lado A");
+            ladoA = Int32.Parse(Console.ReadLine());
+            texto = "\nLado A: " + ladoA;
+            Console.WriteLine("Valor de texto: " + texto);
+
+            Console.WriteLine("\nDigite um valor para o lado B");
+            ladoB = Int32.Parse(Console.ReadLine());
+            texto += "\nLado B: " + ladoB;
+            Console.WriteLine("\nValor de texto: " + texto);
+
+            Console.WriteLine("\nDigite um valor para o lado C");
+            ladoC = Int32.Parse(Console.ReadLine());
+            texto += "\nLado C: " + ladoC;
+            Console.WriteLine("\nValor de texto: " + texto);
+        }
+    }
+}
