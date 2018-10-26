7ef0cf4aadd5116f5545ae4457e813e55ee99515
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-25 20:15:41 -0400

    triangulo objeto

--- triangulo/Program.cs
+++ triangulo/Program.cs
@@ -3,14 +3,45 @@ namespace triangulo
 {
     class Program
     {
-
         static void Main(string[] args)
         {
 
-            //string texto;
-            TRIANGULO triangulo = new TRIANGULO();
+            Triangulo triangulo1 = new Triangulo();
+            Triangulo triangulo2 = new Triangulo();
 
-            TRIANGULO.ladoA = 2;
+            Console.WriteLine("Informe lado A do triangulo 1: ");
+            //triangulo1.ladoA = Int32.Parse(Console.ReadLine());
+            triangulo1.defineValorLadoA(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Informe lado B do triangulo 1: ");
+            //triangulo1.ladoB = Int32.Parse(Console.ReadLine());
+            triangulo1.defineValorLadoB(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Informe lado C do triangulo 1: ");
+            //triangulo1.ladoC = Int32.Parse(Console.ReadLine());
+            triangulo1.defineValorLadoC(Int32.Parse(Console.ReadLine()));
+
+
+            /*Console.WriteLine("Informe lado A do triangulo 2: ");
+            triangulo2.ladoA = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe lado B do triangulo 2: ");
+            triangulo2.ladoB = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe lado C do triangulo 2: ");
+            triangulo2.ladoC = Int32.Parse(Console.ReadLine());*/
+
+
+            /*Console.WriteLine("\nLado A do triangulo 1: " + triangulo1.ladoA + 
+                              "\nLado B do triangulo 1: " + triangulo1.ladoB +
+                              "\nLado C do triangulo 1: " + triangulo1.ladoC);
+            Console.WriteLine("\n\nLado A do triangulo 2: " + triangulo2.ladoA + 
+                              "\nLado B do triangulo 2: " + triangulo2.ladoB +
+                              "\nLado C do triangulo 2: " + triangulo2.ladoC);*/
+
+            Console.WriteLine(triangulo1.pegaValorladoA());
+            Console.WriteLine("Lado B: " + triangulo1.pegaValorladoB());
+            Console.WriteLine("Lado C: " + triangulo1.pegaValorladoC());
 
         }
     }
---
+++ triangulo/Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace triangulo
+{
+    public class Triangulo
+    {
+
+        private int ladoA;
+        private int ladoB;
+        private int ladoC;
+
+        public void defineValorLadoA(int valorDaTelaA){
+            ladoA = valorDaTelaA;
+        }
+
+        public string pegaValorladoA(){
+            return "Lado A: " + ladoA.ToString();
+        }
+
+        public void defineValorLadoB(int valorDaTelaB)
+        {
+            ladoB = valorDaTelaB;
+        }
+
+        public int pegaValorladoB()
+        {
+            return ladoB;
+        }
+
+        public void defineValorLadoC(int valorDaTelaC)
+        {
+            ladoC = valorDaTelaC;
+        }
+
+        public int pegaValorladoC()
+        {
+            return ladoC;
+        }
+
+        public Triangulo()
+        {
+
+        }
+    }
+}
